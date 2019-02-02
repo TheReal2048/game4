b0d7143216c944fc2e045f9b312a6f223c0c250e
Nethaban <30377326+Nethaban@users.noreply.github.com>
2019-02-02 15:46:46 +0000

    3

--- js/game_manager.js
+++ js/game_manager.js
@@ -4,7 +4,7 @@ function GameManager(size, InputManager, Actuator, ScoreManager) {
   this.scoreManager = new ScoreManager;
   this.actuator     = new Actuator;
 
-  this.startTiles   = 2;
+  this.startTiles   = 1;
 
   this.inputManager.on("move", this.move.bind(this));
   this.inputManager.on("restart", this.restart.bind(this));
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.999 ? 1 : Math.random() < 0.999 ? 2 : 3;
+    var value = Math.random() < 0.998 ? 1 : Math.random() < 0.99 ? 2 : 3;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
