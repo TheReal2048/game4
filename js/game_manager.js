1e890b7062b57217b68dd7efc285395d4e8ee692
TheReal2048 <64551777+TheReal2048@users.noreply.github.com>
2020-05-02 16:36:12 -0700

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.999 ? 1 : Math.random() < 0.99 ? 2 : 3;
+    var value = Math.random() < 0.9 ? 1 : Math.random() < 0.5 ? 2 : 0;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -129,8 +129,8 @@ GameManager.prototype.move = function (direction) {
         if (next && next.value === tile.value && !next.mergedFrom || next && next.value == 0 || next && tile.value == 0) {
           
 	        var merged = new Tile(positions.next, tile.value + 1);
-          if(next && next.value == 0) merged.value = tile.value / 2;
-          if(next && tile.value == 0) merged.value = next.value / 2;
+          if(next && next.value == 0) merged.value = tile.value - 1;
+          if(next && tile.value == 0) merged.value = next.value - 1;
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
